16c8d5c5072d11a7d236862304ba06b53d9bd72c
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-04 18:44:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def join_nested_strings(src)
   row_index = 0
   while row_index < src.count do element_index = 0 
     while element_index < src[row_index].count do
-   if src[row_index][inner_index].class == String
+   if src[row_index][element_index].class == String
  end
  element_index +=1
  end
